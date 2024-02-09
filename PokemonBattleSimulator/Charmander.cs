e0774d79362d81f8e4a7e64e8ed83f9c32f2e9e0
Nadir <113099680+CasaDev1@users.noreply.github.com>
2024-02-09 15:45:43 +0100

    charmander class

---
+++ PokemonBattleSimulator/Charmander.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Charmander
+{
+    public string Nickname;
+
+    public string Strength { get; }
+    public string Weakness { get; }
+
+    public Charmander(string nickname, string strength, string weakness)
+    {
+        Nickname = nickname;
+        Strength = strength;
+        Weakness = weakness;
+    }
+
+    // hier maak een  die functie aantal keer een string print
+    public void DoBattleCry(int times)
+    {
+        for (int i = 0; i < times; i++)
+        {
+            Console.WriteLine($"{Nickname} says: {Nickname}!");
+        }
+    }
+}
